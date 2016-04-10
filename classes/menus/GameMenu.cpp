0a4c13e2b628a751d207b236a5cd902b0027c288
Rudolf Cicko <alu0100824780@ull.edu.es>
2016-04-10 14:22:41 +0100

    Changed menu items font color and added some constants

--- classes/menus/GameMenu.cpp
+++ classes/menus/GameMenu.cpp
@@ -4,7 +4,16 @@
 
 using namespace std;
 
+// SCENE
 #define BACKGROUND_COLOR4B Color4B(135, 211, 124, 255)
+
+// MENU ITEMS FONT
+#define MENU_ITEMS_FONT_COLOR Color3B(30, 130, 76)
+#define MENU_ITEM_FONT_SIZE 50
+
+// TRANSITION
+#define TRANSITION_COLOR3B Color3B(255,255,255)
+#define TRANSITION_DELAY 1
 
 USING_NS_CC;
 
@@ -23,18 +32,21 @@ bool GameMenu::init() {
 
     // setFontName es un método estático por lo tanto desde que establezcamos un tipo de fuente
     // todos los objetos menuItemFont que vayamos creando se dibujaran con esa fuente.
+    //    MenuItemFont::setFontName("fonts/AmaticSC-Regular.ttf");
     MenuItemFont::setFontName("fonts/Amatic-Bold.ttf");
-    MenuItemFont::setFontSize(50);
-    auto play     = MenuItemFont::create("Comenzar", CC_CALLBACK_0(GameMenu::play,this));
+    MenuItemFont::setFontSize(MENU_ITEM_FONT_SIZE);
 
-
-    //MenuItemFont::setFontName("fonts/AmaticSC-Regular.ttf");
+    auto play    = MenuItemFont::create("Comenzar", CC_CALLBACK_0(GameMenu::play,this));
     auto settings = MenuItemFont::create("Opciones", CC_CALLBACK_0(GameMenu::settings,this));
     auto quit     = MenuItemFont::create("Salir", CC_CALLBACK_0(GameMenu::quit,this));
 
+    play->setColor(MENU_ITEMS_FONT_COLOR);
+    settings->setColor(MENU_ITEMS_FONT_COLOR);
+    quit->setColor(MENU_ITEMS_FONT_COLOR);
+
     auto *menu = Menu::create(play, settings, quit, NULL);
     menu->alignItemsVertically();
-    this->addChild(menu);
+    addChild(menu);
 
     return true;
 }
@@ -45,7 +57,7 @@ void GameMenu::play(void) {
   auto scene = Level1Scene::createScene();
   auto director = Director::getInstance();
 
-  director->replaceScene(TransitionFade::create(2, scene, Color3B(255,255,255)));
+  director->replaceScene(TransitionFade::create(TRANSITION_DELAY, scene, TRANSITION_COLOR3B));
 }
 
 void GameMenu::settings(void) {
